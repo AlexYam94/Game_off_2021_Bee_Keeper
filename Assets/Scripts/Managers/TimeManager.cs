d39ba8698a444a6001aecfc7a1845808cc66b517
alex.yam <yamminghin@gmail.com>
2021-11-21 23:04:06 +0800

    Implement time function

--- Assets/Scripts/Managers/TimeManager.cs
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -1,36 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimeManager : SaveableEntity, ISaveable
 {
     //Count how many time has left
     //initial time = 1 year = 365 days
-    //1 day = 5 minuts in real time
-    //1 year = 365 * 5 = 1825 minuts = 30.41 hours
+    //1 day = 30 seconds in real time
+    //1 year = 365 * 0.5 = 182.5 minuts = 3.041 hours
     //too long, must have save system
 
-    [SerializeField] private int minutesPerDay = 5;
+    [SerializeField] TextMeshProUGUI dayText;
+    [SerializeField] TextMeshProUGUI timeText;
+    [SerializeField] private float secondsPerDay = 27;
+    [SerializeField] int workingHourStart = 8;
+    [SerializeField] int workingHourEnd = 17;
 
-    private int daysLeft = 365;
+    private int totalDay = 365;
     private float timeLeftToday;
+    private int currentTime;
+    private int currentDay = 1;
+    private float secondsPerHour;
+    private float nextCount;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        timeLeftToday = minutesPerDay;
+        timeLeftToday = secondsPerDay;
+        currentTime = workingHourStart; 
+        secondsPerHour = secondsPerDay / (workingHourEnd - workingHourStart);
+        nextCount = secondsPerDay - secondsPerHour;
     }
 
     private void FixedUpdate()
     {
-        timeLeftToday -= Time.deltaTime;
-        if (timeLeftToday <= 0)
+        timeLeftToday = Mathf.Max(timeLeftToday - Time.deltaTime, 0);
+        if (timeLeftToday < nextCount || timeLeftToday <= 0)
         {
-            timeLeftToday = minutesPerDay;
-            daysLeft -= 1;
+            currentTime = currentTime + 1;
+            nextCount -= secondsPerHour;
+        }
+        timeText.text = String.Format("{0}:00", currentTime);
+        if (nextCount <= 0 && timeLeftToday <= 0)
+        {
+            nextCount = secondsPerDay - secondsPerHour;
+            timeLeftToday = secondsPerDay;
+            currentDay += 1;
+            dayText.text = "Day " + currentDay;
+            currentTime = workingHourStart;
             //TODO: call saveManager to save everything
         }
-        if(daysLeft <= 0)
+        if(currentDay >= totalDay)
         {
             //TODO: load end game scene
         }
---
+++ Assets/Scripts/System/ChangeGameSpeed.cs
@@ -0,0 +1,38 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGameSpeed : MonoBehaviour
+{
+    [SerializeField] GameObject normalSpeedIcon;
+    [SerializeField] GameObject accelerateSpeedIcon;
+
+    private bool isAccelerated = false;
+    public void ToggleAccelerate()
+    {
+        if (!isAccelerated)
+        {
+            accelerateSpeedIcon.SetActive(true);
+            normalSpeedIcon.SetActive(false);
+            Time.timeScale = 2;
+            isAccelerated = true;
+        }
+        else
+        {
+            accelerateSpeedIcon.SetActive(false);
+            normalSpeedIcon.SetActive(true);
+            Time.timeScale = 1;
+            isAccelerated = false;
+        }
+    }
+
+    public void PauseGame()
+    {
+        accelerateSpeedIcon.SetActive(false);
+        normalSpeedIcon.SetActive(true);
+        Time.timeScale = 0;
+        isAccelerated = true;
+
+    }
+}
