2cb3b5e1add08bcd84bdb9f4ed7d52578782da3a
alex.yam <yamminghin@gmail.com>
2021-11-23 23:05:02 +0800

    Add dragable script

--- Assets/Scripts/Items/Dragable.cs
+++ Assets/Scripts/Items/Dragable.cs
@@ -1,9 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Dragable : MonoBehaviour
+public class Dragable : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+
 {
+
+    private Vector3 startPosition;
+    private Transform originalParent;
+
+    // CACHED REFERENCES
+    Canvas parentCanvas;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+        originalParent = transform.parent;
+        // Else won't get the drop event.
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        transform.SetParent(parentCanvas.transform, true);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.pointerCurrentRaycast.worldPosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(originalParent, true);
+        transform.position = startPosition;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
