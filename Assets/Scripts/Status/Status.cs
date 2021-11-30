3f55e1dc3eecc2ca5fd953bb1a1bc6e8a6c3428a
alex.yam <yamminghin@gmail.com>
2021-11-30 22:14:02 +0800

    Update status script and add statuses

--- Assets/Scripts/Status/Status.cs
+++ Assets/Scripts/Status/Status.cs
@@ -7,5 +7,16 @@ public class Status : ScriptableObject
 {
     public int beesToChangePerDay;
     public GameObject icon;
+    public bool isActivate = false;
+    public int lastHappenedDay = 0;
+    public int coolDown = 10;
 
+    public Status(int beesToChangePerDay, GameObject icon, bool isActivate, int lastHappenedDay, int coolDown)
+    {
+        this.beesToChangePerDay = beesToChangePerDay;
+        this.icon = icon;
+        this.isActivate = isActivate;
+        this.lastHappenedDay = lastHappenedDay;
+        this.coolDown = coolDown;
+    }
 }
